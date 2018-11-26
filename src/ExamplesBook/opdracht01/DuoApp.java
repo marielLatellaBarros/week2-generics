16de1b4aeb3702fb2513e6874b23821939e36ec4
Mariel Latella Barros <mariel.latella@gmail.com>
2018-11-26 15:49:11 +0100

    added an Integer type to show that a same class can be used for different object types.

--- src/ExamplesBook/opdracht01/DuoApp.java
+++ src/ExamplesBook/opdracht01/DuoApp.java
@@ -4,5 +4,8 @@ public class DuoApp {
     public static void main(String[] args) {
         Duo<String> sayHiToCosita= new Duo<>("Hola", "cosita!");
         System.out.println(sayHiToCosita);
+
+        Duo<Integer> displayNumbers = new Duo<>(7, 3);
+        System.out.println(displayNumbers);
     }
 }
